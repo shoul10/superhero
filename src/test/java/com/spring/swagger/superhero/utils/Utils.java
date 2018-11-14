3d242b04d25af455370570e4f26891a1442a90fb
shoul10 <mohammed_shl@hotmail.fr>
2018-11-14 18:41:33 -0500

    Refactor asJsonString method

---
+++ src/test/java/com/spring/swagger/superhero/utils/Utils.java
@@ -0,0 +1,19 @@
+package com.spring.swagger.superhero.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Utils {
+	
+	
+	/*
+	 * converts a Java object into JSON representation
+	 */
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
